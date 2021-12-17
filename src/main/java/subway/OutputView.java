f3c329d59e4719a884f3153109d29cc823ccc953
YJGwon <yj970125@gmail.com>
2021-12-17 18:12:29 +0900

    feat: 실행할 기능을 입력받는다

--- src/main/java/subway/OutputView.java
+++ src/main/java/subway/OutputView.java
@@ -5,9 +5,15 @@ public class OutputView {
 	private static final String MAIN_OPTION_SEARCH = "1. 경로 조회";
 	private static final String MAIN_OPTION_QUIT = "Q. 종료";
 
+	private static final String QUESTION_FUNCTION = "## 원하는 기능을 선택하세요.";
+
 	public void printMain() {
 		System.out.println(MAIN_TITLE);
 		System.out.println(MAIN_OPTION_SEARCH);
 		System.out.println(MAIN_OPTION_QUIT);
+	}
+
+	public void askFunction() {
+		System.out.println(QUESTION_FUNCTION);
 	}
 }
--- src/main/java/subway/Program.java
+++ src/main/java/subway/Program.java
@@ -1,10 +1,19 @@
 package subway;
 
+import java.util.Scanner;
+
 public class Program {
 	private final OutputView outputView = new OutputView();
+	private final Scanner scanner;
+
+	public Program(Scanner scanner) {
+		this.scanner = scanner;
+	}
 
 	public void run() {
 		DataInitializer.init();
 		outputView.printMain();
+		outputView.askFunction();
+		scanner.nextLine();
 	}
 }
