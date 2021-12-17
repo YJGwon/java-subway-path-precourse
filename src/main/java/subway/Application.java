382eb206649d8d8e4ca123f4788c41ca83a711f3
YJGwon <yj970125@gmail.com>
2021-12-17 19:42:52 +0900

    feat: 입력값이 B이면 다시 메인으로 돌아간다

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -6,7 +6,11 @@ public class Application {
 	public static void main(String[] args) {
 		final Scanner scanner = new Scanner(System.in);
 		// TODO: 프로그램 구현
+		DataInitializer.init();
+
 		Program program = new Program(scanner);
-		program.run();
+		while (program.isRunning()) {
+			program.run();
+		}
 	}
 }
--- src/main/java/subway/Program.java
+++ src/main/java/subway/Program.java
@@ -7,38 +7,65 @@ public class Program {
 	private final InputValidator inputValidator = new InputValidator();
 	private final Scanner scanner;
 
+	private boolean running = true;
+
 	public Program(Scanner scanner) {
 		this.scanner = scanner;
 	}
 
 	public void run() {
-		DataInitializer.init();
-		outputView.printMain();
-		String chosenFunction = chooseFunction();
-		if (chosenFunction.equals(FunctionOption.QUIT.getCode())) {
-			return;
+		chooseFunction();
+		if (running) {
+			search();
 		}
-		outputView.printSearchOption();
-		chooseSearchOption();
 	}
 
-	private String chooseFunction() {
+	public boolean isRunning() {
+		return running;
+	}
+
+	private void chooseFunction() {
+		outputView.printMain();
+		if (askFunction().equals(FunctionOption.QUIT.getCode())) {
+			running = false;
+		}
+	}
+
+	private void search() {
+		String chosenOptionCode = chooseSearchOption();
+		if (chosenOptionCode.equals(SearchOption.GO_BACK.getCode())) {
+			return;
+		}
+		if (chosenOptionCode.equals(SearchOption.DISTANCE.getCode())) {
+			//TODO: 최단거리 경로 탐색
+		}
+		if (chosenOptionCode.equals(SearchOption.TIME.getCode())) {
+			//TODO: 최단시간 경로 탐색
+		}
+	}
+
+	private String chooseSearchOption() {
+		outputView.printSearchOption();
+		return askSearchOption();
+	}
+
+	private String askFunction() {
 		outputView.askFunction();
 		try {
 			return inputValidator.validateFunction(scanner.nextLine());
 		} catch (IllegalArgumentException e) {
 			outputView.printError(e);
-			return chooseFunction();
+			return askFunction();
 		}
 	}
 
-	private String chooseSearchOption() {
+	private String askSearchOption() {
 		outputView.askFunction();
 		try {
 			return inputValidator.validateSearchOption(scanner.nextLine());
 		} catch (IllegalArgumentException e) {
 			outputView.printError(e);
-			return chooseSearchOption();
+			return askSearchOption();
 		}
 	}
 }
