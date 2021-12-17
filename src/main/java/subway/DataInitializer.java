f0c22def92a56e923ed8a1b3f81566898036f58c
YJGwon <yj970125@gmail.com>
2021-12-17 17:29:55 +0900

    feat: 노선을 초기화한다

--- src/main/java/subway/DataInitializer.java
+++ src/main/java/subway/DataInitializer.java
@@ -3,6 +3,8 @@ package subway;
 import java.util.Arrays;
 import java.util.List;
 
+import subway.domain.Line;
+import subway.domain.LineRepository;
 import subway.domain.Station;
 import subway.domain.StationRepository;
 
@@ -10,14 +12,24 @@ public class DataInitializer {
 	private static final List<String> stationNames = Arrays.asList(
 		"교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"
 	);
+	private static final List<String> lineNames = Arrays.asList(
+		"2호선", "3호선", "신분당선"
+	);
 
 	public static void init() {
-		initStation();
+		initStations();
+		initLines();
 	}
 
-	private static void initStation() {
+	private static void initStations() {
 		for (String stationName : stationNames) {
 			StationRepository.addStation(new Station(stationName));
+		}
+	}
+
+	private static void initLines() {
+		for (String lineName : lineNames) {
+			LineRepository.addLine(new Line(lineName));
 		}
 	}
 }
